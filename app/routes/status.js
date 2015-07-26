c8c61bfe4a508449258b416a935a81899969e6a0
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2015-07-26 23:37:22 +0200

    Added config and routes dirs

---
+++ app/routes/status.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var router = express.Router();              // get an instance of the express Router
+
+// on routes that end in /things
+// ----------------------------------------------------
+router.route('/')
+
+    // get the status (accessed at GET http://localhost:8080/status)
+    .get(function(req, res) {
+        var stat = {"status":"OK"}
+        res.send(stat);
+    });
+
+// export router module
+module.exports = router;

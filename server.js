5e31f45b277c82678f49c5a809ce43e561e5c606
Drasko DRASKOVIC <drasko.draskovic@gmail.com>
2015-11-01 03:25:29 +0100

    Add Mocha, Chai and Supertest combo for testing
    Signed-off-by: Drasko DRASKOVIC <drasko.draskovic@gmail.com>

--- server.js
+++ server.js
@@ -1,4 +1,10 @@
-// server.js
+/**
+ * Copyright (c) Mainflux
+ *
+ * Mainflux server is licensed under an Apache license, version 2.0 license.
+ * All rights not explicitly granted in the Apache license, version 2.0 are reserved.
+ * See the included LICENSE file for more details.
+ */
 
 /**
  * Extrenal configs are kept in the config.js file on the same level
@@ -7,31 +13,32 @@ var config = require('./config/config');
 console.log(config.message);
 
 
-// BASE SETUP
-// =============================================================================
-
-// call the packages we need
+/**
+ * SETUP
+ */
 var express     = require('express');        // call express
 var app         = express();                 // define our app using express
 var bodyParser  = require('body-parser');
 
-// MongoDB
+/** MongoDB */
 var mongoose    = require('mongoose');
-// Docker MongoDB url
+
+/** Docker MongoDB url */
 var docker_mongo_url = process.env.MAINFLUX_MONGODB_1_PORT_27017_TCP_ADDR
 
-mongoose.connect(docker_mongo_url || config.db.path + ':' + config.db.port + '/' + config.db.name); // connect to our database
+/** Connect to DB */
+mongoose.connect(docker_mongo_url || config.db.path + ':' + config.db.port + '/' + config.db.name);
 
-
-// configure app to use bodyParser()
-// this will let us get the data from a POST
+/** Configure app to use bodyParser() */
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
 var port = process.env.PORT || config.port;        // set our port
 
-// ROUTES FOR OUR API
-// =============================================================================
+
+/**
+ * ROUTES
+ */
 app.use('/status', require('./app/routes/status'));
 app.use('/devices', require('./app/routes/devices'));
 app.use('/users', require('./app/routes/users'));
@@ -39,7 +46,15 @@ app.use('/sessions', require('./app/routes/sessions'));
 
 
 
-// START THE SERVER
-// =============================================================================
+
+/**
+ * SERVER START
+ */
 app.listen(port);
 console.log('Magic happens on port ' + port);
+
+
+/**
+ * Export app for testing
+ */
+module.exports = app;

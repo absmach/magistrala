f5bb65d0e4d3f2f6b654687b35971f25b314d4b6
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2019-05-13 14:48:54 +0200

    NOISSUE - Add compile test to CI (#743)
    * Add compile tests to CI script
    
    Signed-off-by: Dušan Borovčanin <dusan.borovcanin@mainflux.com>
    
    * Fix parallelization flag
    
    Signed-off-by: Dušan Borovčanin <dusan.borovcanin@mainflux.com>

--- scripts/ci.sh
+++ scripts/ci.sh
@@ -1,4 +1,5 @@
 # This script contains commands to be executed by the CI tool.
+NPROC=$(nproc)
 
 setup_protoc() {
 	echo "Setting up protoc..."
@@ -32,6 +33,7 @@ setup_mf() {
 			exit 1
 		fi
 	done
+	make -j$NPROC
 }
 
 setup() {
@@ -56,7 +58,7 @@ run_test() {
 push() {
 	if test -n "$BRANCH_NAME" && test "$BRANCH_NAME" = "master"; then
 		echo "Pushing Docker images..."
-		make latest
+		make -j$NPROC latest
 	fi
 }
 

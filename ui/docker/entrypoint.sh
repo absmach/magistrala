8efcaee350cd8ac41ae52808d85cc2502c4dfd63
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2019-07-24 10:44:39 +0000

    NOISSUE - make correct reference to MF_UI_PORT (#804)
    * make correct reference to MF_UI_PORT
    
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>
    
    * remove echo
    
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- ui/docker/entrypoint.sh
+++ ui/docker/entrypoint.sh
@@ -1,9 +1,9 @@
 #!/bin/ash
 
-if [ -n "$UI_PORT" ]; then
-    sed -i -e "s/UI_PORT/$UI_PORT/" /etc/nginx/conf.d/default.conf
+if [ -n "$MF_UI_PORT" ]; then
+    sed -i -e "s/MF_UI_PORT/$MF_UI_PORT/" /etc/nginx/conf.d/default.conf
 else
-    sed -i -e "s/UI_PORT/3000/" /etc/nginx/conf.d/default.conf
+    sed -i -e "s/MF_UI_PORT/3000/" /etc/nginx/conf.d/default.conf
 fi
 
 exec nginx -g "daemon off;"

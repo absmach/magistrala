2d1347eee38bb7ebbd6a24d6f00a8aa87d3729cc
Ivan Milošević <iva@blokovi.com>
2019-08-30 16:23:28 +0200

    Switch secure of WS connection according to secure of http connection of UI (#829)
    Signed-off-by: Ivan Milošević <iva@blokovi.com>

--- ui/src/Websocket.js
+++ ui/src/Websocket.js
@@ -6,7 +6,11 @@ MF.log = function(msg) {
 }
 
 MF.url = function(data) {
-    return 'wss://' + window.location.hostname + '/ws/channels/' + data.channelid + '/messages?authorization=' + data.thingkey
+    var wsProtocol = 'ws';
+    if (window.location.protocol == 'https:') {
+        wsProtocol = 'wss'
+    }
+    return wsProtocol + '://' + window.location.hostname + '/ws/channels/' + data.channelid + '/messages?authorization=' + data.thingkey
 }
 
 app.ports.connectWebsocket.subscribe(function(data) {

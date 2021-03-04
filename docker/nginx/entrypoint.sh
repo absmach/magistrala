259950b009e844bf24eb1c41756caa3f36641da2
Mirko Teodorovic <mirko.teodorovic@gmail.com>
2021-03-04 19:02:59 +0100

    NOISSUE - Add missing auth port in nginx enrypoint.sh (#1380)
    * remove owner id
    
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>
    
    * fix auth http port
    
    Signed-off-by: Mirko Teodorovic <mirko.teodorovic@gmail.com>

--- docker/nginx/entrypoint.sh
+++ docker/nginx/entrypoint.sh
@@ -15,6 +15,7 @@ envsubst '
     ${MF_HTTP_ADAPTER_PORT}
     ${MF_NGINX_MQTT_PORT}
     ${MF_NGINX_MQTTS_PORT}
+    ${MF_AUTH_HTTP_PORT}
     ${MF_WS_ADAPTER_PORT}' < /etc/nginx/nginx.conf.template > /etc/nginx/nginx.conf
 
 exec nginx -g "daemon off;"

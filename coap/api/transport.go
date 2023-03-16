d4fc7bab07d99ca2cf242938801c3d75eb123232
b1ackd0t <blackd0t@protonmail.com>
2023-03-16 13:48:57 +0300

    NOISSUE - Fix Send Response for Publishing Message (#1740)
    * Initial Commit: Send Response for Publishing Message
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    * Use Defer
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    
    ---------
    
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>

--- coap/api/transport.go
+++ coap/api/transport.go
@@ -79,19 +79,17 @@ func handler(w mux.ResponseWriter, m *mux.Message) {
 		Context: m.Context,
 		Options: make(message.Options, 0, 16),
 	}
-
+	defer sendResp(w, &resp)
 	msg, err := decodeMessage(m)
 	if err != nil {
 		logger.Warn(fmt.Sprintf("Error decoding message: %s", err))
 		resp.Code = codes.BadRequest
-		sendResp(w, &resp)
 		return
 	}
 	key, err := parseKey(m)
 	if err != nil {
 		logger.Warn(fmt.Sprintf("Error parsing auth: %s", err))
 		resp.Code = codes.Unauthorized
-		sendResp(w, &resp)
 		return
 	}
 	switch m.Code {
@@ -114,7 +112,6 @@ func handler(w mux.ResponseWriter, m *mux.Message) {
 		default:
 			resp.Code = codes.InternalServerError
 		}
-		sendResp(w, &resp)
 	}
 }
 

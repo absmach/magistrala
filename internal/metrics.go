3b84258a0ce034b615be0eb63bee297439ecce40
charlie-jangala <129748315+charlie-jangala@users.noreply.github.com>
2023-04-27 23:26:04 +0100

    Add Objectives field to Summary metric instantiation (#1772)
    Signed-off-by: Charlie Evans <charlie@janga.la>

--- internal/metrics.go
+++ internal/metrics.go
@@ -16,10 +16,11 @@ func MakeMetrics(namespace, subsystem string) (*kitprometheus.Counter, *kitprome
 		Help:      "Number of requests received.",
 	}, []string{"method"})
 	latency := kitprometheus.NewSummaryFrom(stdprometheus.SummaryOpts{
-		Namespace: namespace,
-		Subsystem: subsystem,
-		Name:      "request_latency_microseconds",
-		Help:      "Total duration of requests in microseconds.",
+		Namespace:  namespace,
+		Subsystem:  subsystem,
+		Objectives: map[float64]float64{0.5: 0.05, 0.9: 0.01, 0.99: 0.001},
+		Name:       "request_latency_microseconds",
+		Help:       "Total duration of requests in microseconds.",
 	}, []string{"method"})
 
 	return counter, latency

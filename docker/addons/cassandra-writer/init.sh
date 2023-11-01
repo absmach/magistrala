7922ce80bab202391df5f92fd86f5d097a7f8c1e
Dušan Borovčanin <dusan.borovcanin@mainflux.com>
2023-11-01 19:16:27 +0100

    Fix Cassandra Writer setup (#23)
    Signed-off-by: dborovcanin <dusan.borovcanin@abstractmachines.fr>
    Co-authored-by: dborovcanin <dusan.borovcanin@abstractmachines.fr>

--- docker/addons/cassandra-writer/init.sh
+++ docker/addons/cassandra-writer/init.sh
@@ -1,8 +1,8 @@
 #!/usr/bin/env bash
-until printf "" 2>>/dev/null >>/dev/tcp/mainflux-cassandra/9042; do
+until printf "" 2>>/dev/null >>/dev/tcp/magistrala-cassandra/9042; do
     sleep 5;
     echo "Waiting for cassandra...";
 done
 
 echo "Creating keyspace and table..."
-cqlsh mainflux-cassandra  -e "CREATE KEYSPACE IF NOT EXISTS mainflux WITH replication = {'class':'SimpleStrategy','replication_factor':'1'};"
+cqlsh magistrala-cassandra  -e "CREATE KEYSPACE IF NOT EXISTS magistrala WITH replication = {'class':'SimpleStrategy','replication_factor':'1'};"

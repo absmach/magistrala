3a27cd8fc31314166f2ee927f552da6f4c40f92c
Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>
2023-12-15 13:10:58 +0300

    Update API test workflow and schemas
    Signed-off-by: Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>
    Signed-off-by: rodneyosodo <blackd0t@protonmail.com>
    Signed-off-by: Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>

--- things/service.go
+++ things/service.go
@@ -241,7 +241,7 @@ func (svc service) listUserThingPermission(ctx context.Context, userID, thingID 
 		ObjectType:  auth.ThingType,
 	})
 	if err != nil {
-		return []string{}, err
+		return []string{}, errors.Wrap(svcerr.ErrAuthorization, err)
 	}
 	return lp.GetPermissions(), nil
 }
@@ -607,7 +607,7 @@ func (svc *service) authorize(ctx context.Context, domainID, subjType, subjKind,
 	}
 	res, err := svc.auth.Authorize(ctx, req)
 	if err != nil {
-		return "", err
+		return "", errors.Wrap(errors.ErrAuthorization, err)
 	}
 	if !res.GetAuthorized() {
 		return "", errors.Wrap(svcerr.ErrAuthorization, err)

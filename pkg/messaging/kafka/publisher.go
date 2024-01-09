6ec15a701d0d50f986c016a3715dbc2a17cd4d98
Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>
2024-01-09 17:33:13 +0300

    Migrate from mainflux to magistrala
    Signed-off-by: Rodney Osodo <28790446+rodneyosodo@users.noreply.github.com>

--- pkg/messaging/kafka/publisher.go
+++ pkg/messaging/kafka/publisher.go
@@ -1,4 +1,4 @@
-// Copyright (c) Mainflux
+// Copyright (c) Abstract Machines
 // SPDX-License-Identifier: Apache-2.0
 
 package kafka
@@ -9,7 +9,7 @@ import (
 	"strings"
 	"sync"
 
-	"github.com/mainflux/mainflux/pkg/messaging"
+	"github.com/absmach/magistrala/pkg/messaging"
 	"github.com/segmentio/kafka-go"
 	"google.golang.org/protobuf/proto"
 )

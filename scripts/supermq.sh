da415939e1b0771cd4c0ace8aefa9509ae276c81
Felix Gateru <felix.gateru@gmail.com>
2025-03-17 14:51:10 +0300

    refactor: update script to use sparse checkout and move docker files into ./docker/supermq-docker
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

--- scripts/supermq.sh
+++ scripts/supermq.sh
@@ -10,10 +10,9 @@ set -e
 set -o pipefail
 
 REPO_URL=https://github.com/absmach/supermq
-TEMP_DIR="smq_temp"
-ZIP_FILE="smq.zip"
+TEMP_DIR="supermq"
 DOCKER_FOLDER="docker"
-DEST_FOLDER="../supermq"
+DEST_FOLDER="../../docker/supermq-docker"
 
 SCRIPT_DIR="$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd)"
 cd "$SCRIPT_DIR" || exit 1
@@ -24,18 +23,18 @@ if [ -n "$(git status --porcelain)" ]; then
 fi
 
 cleanup() {
-    rm -rf "$TEMP_DIR" "$ZIP_FILE"
+    rm -rf "$TEMP_DIR" "../$TEMP_DIR"
 }
 trap cleanup EXIT
 
-curl -s -L "$REPO_URL/archive/refs/heads/main.zip" -o "$ZIP_FILE"
-mkdir -p "$TEMP_DIR"
-unzip -qq "$ZIP_FILE" -d "$TEMP_DIR"
-
-EXTRACTED_FOLDER=$(find "$TEMP_DIR" -mindepth 1 -maxdepth 1 -type d)
+git clone --depth 1 --filter=blob:none --sparse "$REPO_URL"
+cd "$TEMP_DIR"
+git sparse-checkout set "$DOCKER_FOLDER"
 
 if [ -d "$DEST_FOLDER" ]; then
     rm -r "$DEST_FOLDER"
 fi
 mkdir -p "$DEST_FOLDER"
-mv -f "$EXTRACTED_FOLDER/$DOCKER_FOLDER" "$DEST_FOLDER/$DOCKER_FOLDER"
+mv -f "$DOCKER_FOLDER"/* "$DEST_FOLDER"
+cd ..
+rm -rf "$TEMP_DIR"

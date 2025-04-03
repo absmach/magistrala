c63a101001a26b06ffed33ea8b5bae356fe172d5
ianmuchyri <ianmuchiri8@gmail.com>
2025-04-03 15:20:59 +0300

    update postgres writer
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- cmd/postgres-writer/main.go
+++ cmd/postgres-writer/main.go
@@ -34,19 +34,19 @@ import (
 
 const (
 	svcName        = "postgres-writer"
-	envPrefixDB    = "SMQ_POSTGRES_"
-	envPrefixHTTP  = "SMQ_POSTGRES_WRITER_HTTP_"
+	envPrefixDB    = "MG_POSTGRES_"
+	envPrefixHTTP  = "MG_POSTGRES_WRITER_HTTP_"
 	defDB          = "messages"
 	defSvcHTTPPort = "9010"
 )
 
 type config struct {
-	LogLevel      string  `env:"SMQ_POSTGRES_WRITER_LOG_LEVEL"     envDefault:"info"`
-	ConfigPath    string  `env:"SMQ_POSTGRES_WRITER_CONFIG_PATH"   envDefault:"/config.toml"`
+	LogLevel      string  `env:"MG_POSTGRES_WRITER_LOG_LEVEL"     envDefault:"info"`
+	ConfigPath    string  `env:"MG_POSTGRES_WRITER_CONFIG_PATH"   envDefault:"/config.toml"`
 	BrokerURL     string  `env:"SMQ_MESSAGE_BROKER_URL"            envDefault:"nats://localhost:4222"`
 	JaegerURL     url.URL `env:"SMQ_JAEGER_URL"                    envDefault:"http://localhost:4318/v1/traces"`
 	SendTelemetry bool    `env:"SMQ_SEND_TELEMETRY"                envDefault:"true"`
-	InstanceID    string  `env:"SMQ_POSTGRES_WRITER_INSTANCE_ID"   envDefault:""`
+	InstanceID    string  `env:"MG_POSTGRES_WRITER_INSTANCE_ID"   envDefault:""`
 	TraceRatio    float64 `env:"SMQ_JAEGER_TRACE_RATIO"            envDefault:"1.0"`
 }
 

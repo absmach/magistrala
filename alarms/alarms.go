ee553db8ed568a3f3983481fa49a53da06f42cc0
Rodney Osodo <socials@rodneyosodo.com>
2025-04-10 14:40:19 +0300

    feat(alarms): add acknowledged field
    Signed-off-by: Rodney Osodo <socials@rodneyosodo.com>

--- alarms/alarms.go
+++ alarms/alarms.go
@@ -21,24 +21,26 @@ type Metadata map[string]interface{}
 
 // Alarm represents an alarm instance.
 type Alarm struct {
-	ID          string    `json:"id"`
-	RuleID      string    `json:"rule_id"`
-	Status      Status    `json:"status"`
-	Measurement string    `json:"measurement"`
-	Value       string    `json:"value"`
-	Unit        string    `json:"unit"`
-	Cause       string    `json:"cause"`
-	Severity    uint8     `json:"severity"`
-	DomainID    string    `json:"domain_id"`
-	AssigneeID  string    `json:"assignee_id"`
-	CreatedAt   time.Time `json:"created_at"`
-	UpdatedAt   time.Time `json:"updated_at"`
-	UpdatedBy   string    `json:"updated_by"`
-	AssignedAt  time.Time `json:"assigned_at,omitempty"`
-	AssignedBy  string    `json:"assigned_by,omitempty"`
-	ResolvedAt  time.Time `json:"resolved_at,omitempty"`
-	ResolvedBy  string    `json:"resolved_by,omitempty"`
-	Metadata    Metadata  `json:"metadata,omitempty"`
+	ID             string    `json:"id"`
+	RuleID         string    `json:"rule_id"`
+	Status         Status    `json:"status"`
+	Measurement    string    `json:"measurement"`
+	Value          string    `json:"value"`
+	Unit           string    `json:"unit"`
+	Cause          string    `json:"cause"`
+	Severity       uint8     `json:"severity"`
+	DomainID       string    `json:"domain_id"`
+	AssigneeID     string    `json:"assignee_id"`
+	CreatedAt      time.Time `json:"created_at"`
+	UpdatedAt      time.Time `json:"updated_at"`
+	UpdatedBy      string    `json:"updated_by"`
+	AssignedAt     time.Time `json:"assigned_at,omitempty"`
+	AssignedBy     string    `json:"assigned_by,omitempty"`
+	AcknowledgedAt time.Time `json:"acknowledged_at,omitempty"`
+	AcknowledgedBy string    `json:"acknowledged_by,omitempty"`
+	ResolvedAt     time.Time `json:"resolved_at,omitempty"`
+	ResolvedBy     string    `json:"resolved_by,omitempty"`
+	Metadata       Metadata  `json:"metadata,omitempty"`
 }
 
 type AlarmsPage struct {
@@ -49,17 +51,17 @@ type AlarmsPage struct {
 }
 
 type PageMetadata struct {
-	Offset     uint64    `json:"offset"      db:"offset"`
-	Limit      uint64    `json:"limit"       db:"limit"`
-	DomainID   string    `json:"domain_id"   db:"domain_id"`
-	RuleID     string    `json:"rule_id"     db:"rule_id"`
-	Status     Status    `json:"status"      db:"status"`
-	AssigneeID string    `json:"assignee_id" db:"assignee_id"`
-	Severity   uint8     `json:"severity"    db:"severity"`
-	UpdatedBy  string    `json:"updated_by"  db:"updated_by"`
-	ResolvedBy string    `json:"resolved_by" db:"resolved_by"`
-	UpdatedAt  time.Time `json:"updated_at"  db:"updated_at"`
-	ResolvedAt time.Time `json:"resolved_at" db:"resolved_at"`
+	Offset         uint64 `json:"offset"          db:"offset"`
+	Limit          uint64 `json:"limit"           db:"limit"`
+	DomainID       string `json:"domain_id"       db:"domain_id"`
+	RuleID         string `json:"rule_id"         db:"rule_id"`
+	Status         Status `json:"status"          db:"status"`
+	AssigneeID     string `json:"assignee_id"     db:"assignee_id"`
+	Severity       uint8  `json:"severity"        db:"severity"`
+	UpdatedBy      string `json:"updated_by"      db:"updated_by"`
+	AssignedBy     string `json:"assigned_by"     db:"assigned_by"`
+	AcknowledgedBy string `json:"acknowledged_by" db:"acknowledged_by"`
+	ResolvedBy     string `json:"resolved_by"     db:"resolved_by"`
 }
 
 func (a Alarm) Validate() error {

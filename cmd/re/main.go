a3868f94af21de29310584f519bd6f6dd44160b4
ianmuchyri <ianmuchiri8@gmail.com>
2025-04-11 18:01:37 +0300

    remove extra variable
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- cmd/re/main.go
+++ cmd/re/main.go
@@ -56,11 +56,6 @@ var (
 	writersPrefix = "writers"
 )
 
-var (
-	alarmsPrefix  = "alarms"
-	writersPrefix = "writers"
-)
-
 type config struct {
 	LogLevel         string        `env:"MG_RE_LOG_LEVEL"           envDefault:"info"`
 	InstanceID       string        `env:"MG_RE_INSTANCE_ID"         envDefault:""`

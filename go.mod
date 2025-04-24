254e2bc9eddb558efcf4d2799eb7afff34faa6f7
nyagamunene <stevenyaga2014@gmail.com>
2025-04-24 21:39:21 +0300

    fix generate PDF and CSV
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- go.mod
+++ go.mod
@@ -4,7 +4,6 @@ go 1.24.2
 
 require (
 	github.com/0x6flab/namegenerator v1.4.0
-	github.com/SebastiaanKlippert/go-wkhtmltopdf v1.9.3
 	github.com/absmach/callhome v0.14.0
 	github.com/absmach/supermq v0.16.1-0.20250425120637-5ea3cbd0147a
 	github.com/authzed/authzed-go v1.4.0
@@ -24,6 +23,7 @@ require (
 	github.com/jackc/pgtype v1.14.4
 	github.com/jackc/pgx/v5 v5.7.4
 	github.com/jmoiron/sqlx v1.4.0
+	github.com/johnfercher/maroto v1.0.0
 	github.com/ory/dockertest/v3 v3.12.0
 	github.com/pelletier/go-toml v1.9.5
 	github.com/prometheus/client_golang v1.22.0
@@ -44,7 +44,12 @@ require (
 	moul.io/http2curl v1.0.0
 )
 
-require github.com/lib/pq v1.10.9 // indirect
+require (
+	github.com/boombuler/barcode v1.0.1 // indirect
+	github.com/jung-kurt/gofpdf v1.16.2 // indirect
+	github.com/lib/pq v1.10.9 // indirect
+	github.com/ruudk/golang-pdf417 v0.0.0-20201230142125-a7e3863a1245 // indirect
+)
 
 require (
 	dario.cat/mergo v1.0.0 // indirect
--- re/generator.go
+++ re/generator.go
@@ -7,201 +7,482 @@ import (
 	"bytes"
 	"encoding/csv"
 	"fmt"
-	"html/template"
 	"sort"
 	"time"
 
-	"github.com/SebastiaanKlippert/go-wkhtmltopdf"
+	"github.com/absmach/supermq/pkg/errors"
+	svcerr "github.com/absmach/supermq/pkg/errors/service"
 	"github.com/absmach/supermq/pkg/transformers/senml"
+	"github.com/johnfercher/maroto/pkg/color"
+	"github.com/johnfercher/maroto/pkg/consts"
+	"github.com/johnfercher/maroto/pkg/pdf"
+	"github.com/johnfercher/maroto/pkg/props"
 )
 
-const reportTemplate = `
-<!DOCTYPE html>
-<html>
-<head>
-    <style>
-        body {
-            font-family: Arial, sans-serif;
-            margin: 40px;
-        }
-        h1 {
-            color: #333;
-            font-size: 24px;
-        }
-        h2 {
-            color: #555;
-            font-size: 18px;
-            margin-top: 20px;
-        }
-        .report-info {
-            margin-bottom: 20px;
-        }
-        table {
-            width: 100%;
-            border-collapse: collapse;
-            margin-top: 20px;
-        }
-        th {
-            background-color: #f2f2f2;
-            padding: 8px;
-            text-align: left;
-            border: 1px solid #ddd;
-        }
-        td {
-            padding: 8px;
-            border: 1px solid #ddd;
-        }
-        tr:nth-child(even) {
-            background-color: #f9f9f9;
-        }
-        .page-break {
-            page-break-after: always;
-        }
-    </style>
-</head>
-<body>
-    {{range $reportIndex, $report := .Reports}}
-    <div {{if lt $reportIndex (subtract (len $.Reports) 1)}}class="page-break"{{end}}>
-        <h1>Metrics Report</h1>
-        <div class="report-info">
-            <h2>Report Information</h2>
-            <p><strong>Metric:</strong> {{$report.Metric.Name}}</p>
-            <p><strong>Device ID:</strong> {{$report.Metric.ClientID}}</p>
-            <p><strong>Channel ID:</strong> {{$report.Metric.ChannelID}}</p>
-            {{if $report.Metric.Subtopic}}
-            <p><strong>Subtopic:</strong> {{$report.Metric.Subtopic}}</p>
-            {{end}}
-        </div>
+// func generatePDFReport(reports []Report) ([]byte, error) {
+// 	m := pdf.NewMaroto(consts.Portrait, consts.A4)
 
-        <table>
-            <thead>
-                <tr>
-                    <th>Time</th>
-                    <th>Metric Name</th>
-                    <th>Value</th>
-                    <th>Unit</th>
-                    <th>Subtopic</th>
-                </tr>
-            </thead>
-            <tbody>
-                {{range $report.SortedMessages}}
-                <tr>
-                    <td>{{.FormattedTime}}</td>
-                    <td>{{$report.Metric.Name}}</td>
-                    <td>{{.FormattedValue}}</td>
-                    <td>{{.Unit}}</td>
-                    <td>{{.Subtopic}}</td>
-                </tr>
-                {{end}}
-            </tbody>
-        </table>
-    </div>
-    {{end}}
-</body>
-</html>
-`
+// 	m.SetPageMargins(10, 15, 10)
+
+// 	headerBlue := color.Color{Red: 64, Green: 126, Blue: 201}
+// 	footerBlue := color.Color{Red: 64, Green: 126, Blue: 201}
+// 	alternateRow := color.Color{Red: 240, Green: 240, Blue: 240}
+// 	white := color.NewWhite()
+// 	tableHeaderBg := color.Color{Red: 220, Green: 220, Blue: 220}
+// 	titleColor := color.Color{Red: 0, Green: 51, Blue: 102}
+
+// 	m.RegisterHeader(func() {
+// 		m.Row(15, func() {
+// 			m.Col(3, func() {})
+
+// 			m.Col(6, func() {
+// 				m.Text("Magistrala IoT Report", props.Text{
+// 					Size:  16,
+// 					Style: consts.Bold,
+// 					Color: headerBlue,
+// 					Align: consts.Center,
+// 					Top:   3,
+// 				})
+// 			})
+
+// 			m.Col(3, func() {
+// 				m.Text(time.Now().Format("02 Jan 2006"), props.Text{
+// 					Size:  10,
+// 					Align: consts.Right,
+// 					Color: titleColor,
+// 					Top:   5,
+// 				})
+// 			})
+// 		})
+
+// 		m.SetBackgroundColor(headerBlue)
+// 		m.Row(1, func() { m.Col(12, func() {}) })
+// 		m.SetBackgroundColor(white)
+// 		m.Row(5, func() {})
+// 	})
+
+// 	m.RegisterFooter(func() {
+// 		currentPage := m.GetCurrentPage()
+
+// 		m.SetBackgroundColor(footerBlue)
+// 		m.Row(1, func() { m.Col(12, func() {}) })
+// 		m.SetBackgroundColor(white)
+
+// 		m.Row(10, func() {
+// 			m.Col(12, func() {
+// 				m.Text(fmt.Sprintf("Page %d", currentPage), props.Text{
+// 					Size:  9,
+// 					Align: consts.Center,
+// 					Color: titleColor,
+// 					Top:   3,
+// 				})
+// 			})
+// 		})
+// 	})
+
+// 	headers := []string{"Time", "Metric Name", "Value", "Unit", "Subtopic"}
+// 	widths := []uint{4, 2, 2, 2, 2}
+
+// 	for i, report := range reports {
+// 		if i > 0 {
+// 			m.AddPage()
+// 		}
+
+// 		m.Row(8, func() {
+// 			m.Col(12, func() {
+// 				m.Text(fmt.Sprintf("Device ID: %s", report.Metric.ClientID), props.Text{
+// 					Size:  12,
+// 					Style: consts.Bold,
+// 				})
+// 			})
+// 		})
+
+// 		m.Row(8, func() {
+// 			m.Col(12, func() {
+// 				m.Text(fmt.Sprintf("Channel ID: %s", report.Metric.ChannelID), props.Text{
+// 					Size:  12,
+// 					Style: consts.Bold,
+// 				})
+// 			})
+// 		})
+
+// 		m.Row(5, func() {})
+
+// 		m.SetBackgroundColor(tableHeaderBg)
+// 		m.Row(10, func() {
+// 			for i, header := range headers {
+// 				m.Col(widths[i], func() {
+// 					m.Text(header, props.Text{
+// 						Size:  11,
+// 						Style: consts.Bold,
+// 						Align: consts.Center,
+// 						Top:   2,
+// 						Color: titleColor,
+// 					})
+// 				})
+// 			}
+// 		})
+// 		m.SetBackgroundColor(white)
+
+// 		useAlternateColor := false
+// 		for _, msg := range report.Messages {
+// 			if useAlternateColor {
+// 				m.SetBackgroundColor(alternateRow)
+// 			}
+
+// 			m.Row(8, func() {
+// 				m.Col(widths[0], func() {
+// 					m.Text(formatTime(msg.Time), props.Text{
+// 						Size:  10,
+// 						Align: consts.Left,
+// 						Top:   1,
+// 					})
+// 				})
+
+// 				m.Col(widths[1], func() {
+// 					m.Text(report.Metric.Name, props.Text{
+// 						Size:  10,
+// 						Align: consts.Center,
+// 						Top:   1,
+// 					})
+// 				})
+
+// 				m.Col(widths[2], func() {
+// 					m.Text(formatValue(msg), props.Text{
+// 						Size:  10,
+// 						Align: consts.Center,
+// 						Top:   1,
+// 					})
+// 				})
+
+// 				m.Col(widths[3], func() {
+// 					m.Text(msg.Unit, props.Text{
+// 						Size:  10,
+// 						Align: consts.Center,
+// 						Top:   1,
+// 					})
+// 				})
+
+// 				m.Col(widths[4], func() {
+// 					m.Text(msg.Subtopic, props.Text{
+// 						Size:  10,
+// 						Align: consts.Center,
+// 						Top:   1,
+// 					})
+// 				})
+// 			})
+
+// 			useAlternateColor = !useAlternateColor
+// 			m.SetBackgroundColor(white)
+// 		}
+// 	}
+
+// 	buf, err := m.Output()
+// 	if err != nil {
+// 		return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
+// 	}
+
+// 	return buf.Bytes(), nil
+// }
 
 func generatePDFReport(reports []Report) ([]byte, error) {
-	type MessageWithFormatting struct {
-		senml.Message
-		FormattedTime  string
-		FormattedValue string
-	}
+	m := pdf.NewMaroto(consts.Portrait, consts.A4)
+	m.SetPageMargins(10, 15, 10)
 
-	type ReportWithSortedMessages struct {
-		Metric         Metric
-		SortedMessages []MessageWithFormatting
-	}
+	primaryColor := color.Color{Red: 41, Green: 128, Blue: 185}   // Blue
+	secondaryColor := color.Color{Red: 26, Green: 82, Blue: 118}  // Darker blue
+	subtleColor := color.Color{Red: 189, Green: 195, Blue: 199}   // Light gray
+	tableHeaderBg := color.Color{Red: 236, Green: 240, Blue: 241} // Very light gray
+	alternateRow := color.Color{Red: 245, Green: 247, Blue: 249}  // Even lighter gray
+	textPrimary := color.Color{Red: 44, Green: 62, Blue: 80}      // Dark blue-gray
+	textSecondary := color.Color{Red: 127, Green: 140, Blue: 141} // Medium gray
+	white := color.NewWhite()
 
-	type TemplateData struct {
-		Reports []ReportWithSortedMessages
-	}
+	m.RegisterHeader(func() {
+		m.SetBackgroundColor(primaryColor)
+		m.Row(2, func() { m.Col(12, func() {}) })
+		m.SetBackgroundColor(white)
 
-	templateFuncs := template.FuncMap{
-		"subtract": func(a, b int) int {
-			return a - b
-		},
-	}
+		m.Row(20, func() {
+			m.Col(2, func() {})
 
-	templateReports := make([]ReportWithSortedMessages, 0, len(reports))
+			m.Col(8, func() {
+				m.Text("Magistrala IoT Report", props.Text{
+					Size:  20,
+					Style: consts.Bold,
+					Color: primaryColor,
+					Align: consts.Center,
+					Top:   6,
+				})
+			})
 
-	for _, report := range reports {
-		if len(report.Messages) == 0 {
-			continue
-		}
-
-		sort.Slice(report.Messages, func(i, j int) bool {
-			return report.Messages[i].Time < report.Messages[j].Time
+			m.Col(2, func() {
+				m.Text(time.Now().Format("02 Jan 2006"), props.Text{
+					Size:  10,
+					Style: consts.Italic,
+					Align: consts.Right,
+					Color: textSecondary,
+					Top:   8,
+				})
+			})
 		})
 
-		formattedMessages := make([]MessageWithFormatting, len(report.Messages))
-		for i, msg := range report.Messages {
-			timeStr := time.Unix(int64(msg.Time), 0).Format("2006-01-02 15:04:05")
-			if msg.Time > 9999999999 {
-				timeStr = time.Unix(0, int64(msg.Time)*1000000).Format("2006-01-02 15:04:05")
-			}
+		m.SetBackgroundColor(subtleColor)
+		m.Row(0.5, func() { m.Col(12, func() {}) })
+		m.SetBackgroundColor(white)
+		m.Row(0.25, func() {})
+		m.SetBackgroundColor(subtleColor)
+		m.Row(0.25, func() { m.Col(12, func() {}) })
+		m.SetBackgroundColor(white)
 
-			var valueStr string
-			if msg.Value != nil {
-				valueStr = fmt.Sprintf("%.2f", *msg.Value)
-			} else if msg.StringValue != nil {
-				valueStr = *msg.StringValue
-			} else if msg.BoolValue != nil {
-				valueStr = fmt.Sprintf("%v", *msg.BoolValue)
-			} else if msg.DataValue != nil {
-				valueStr = *msg.DataValue
-			} else {
-				valueStr = "N/A"
-			}
+		m.Row(5, func() {})
+	})
 
-			formattedMessages[i] = MessageWithFormatting{
-				Message:        msg,
-				FormattedTime:  timeStr,
-				FormattedValue: valueStr,
-			}
+	m.RegisterFooter(func() {
+		currentPage := m.GetCurrentPage()
+
+		m.Row(5, func() {})
+		m.SetBackgroundColor(subtleColor)
+		m.Row(0.25, func() { m.Col(12, func() {}) })
+		m.SetBackgroundColor(white)
+		m.Row(0.25, func() {})
+		m.SetBackgroundColor(subtleColor)
+		m.Row(0.5, func() { m.Col(12, func() {}) })
+		m.SetBackgroundColor(white)
+
+		m.Row(10, func() {
+			m.Col(4, func() {
+				m.Text("Generated: "+time.Now().Format("15:04:05"), props.Text{
+					Size:  8,
+					Style: consts.Italic,
+					Align: consts.Left,
+					Color: textSecondary,
+					Top:   3,
+				})
+			})
+
+			m.Col(4, func() {
+				m.Text(fmt.Sprintf("Page %d", currentPage+1), props.Text{
+					Size:  9,
+					Style: consts.Bold,
+					Align: consts.Center,
+					Color: textPrimary,
+					Top:   3,
+				})
+			})
+
+			m.Col(4, func() {
+				m.Text("Magistrala System", props.Text{
+					Size:  8,
+					Style: consts.Italic,
+					Align: consts.Right,
+					Color: textSecondary,
+					Top:   3,
+				})
+			})
+		})
+	})
+
+	headers := []string{"Time", "Metric Name", "Value", "Unit", "Subtopic"}
+	widths := []uint{4, 2, 2, 2, 2}
+
+	for i, report := range reports {
+		if i > 0 {
+			m.AddPage()
 		}
 
-		templateReports = append(templateReports, ReportWithSortedMessages{
-			Metric:         report.Metric,
-			SortedMessages: formattedMessages,
+		m.Row(0.5, func() {
+			m.Col(1, func() {})
 		})
+		m.SetBackgroundColor(white)
+
+		m.Row(10, func() {
+			m.Col(12, func() {
+				m.Text("Metrics", props.Text{
+					Size:  16,
+					Style: consts.Bold,
+					Color: secondaryColor,
+					Top:   2,
+				})
+			})
+		})
+
+		m.SetBackgroundColor(alternateRow)
+		m.Row(0.5, func() { m.Col(12, func() {}) })
+
+		m.Row(8, func() {
+			m.Col(2, func() {
+				m.Text("Device ID:	", props.Text{
+					Size:  11,
+					Style: consts.Bold,
+					Align: consts.Left,
+					Color: textPrimary,
+					Top:   1,
+				})
+			})
+
+			m.Col(10, func() {
+				m.Text(report.Metric.ClientID, props.Text{
+					Size:  11,
+					Style: consts.Italic,
+					Color: textPrimary,
+					Top:   1,
+				})
+			})
+		})
+
+		m.Row(8, func() {
+			m.Col(2, func() {
+				m.Text("Channel ID:	", props.Text{
+					Size:  11,
+					Style: consts.Bold,
+					Align: consts.Left,
+					Color: textPrimary,
+					Top:   1,
+				})
+			})
+
+			m.Col(10, func() {
+				m.Text(report.Metric.ChannelID, props.Text{
+					Size:  11,
+					Style: consts.Italic,
+					Color: textPrimary,
+					Top:   1,
+				})
+			})
+		})
+
+		m.SetBackgroundColor(alternateRow)
+		m.Row(0.5, func() { m.Col(12, func() {}) })
+		m.SetBackgroundColor(white)
+
+		m.Row(10, func() {
+			m.Col(12, func() {
+				m.Text(fmt.Sprintf("Total Records: %d", len(report.Messages)), props.Text{
+					Size:  10,
+					Style: consts.Italic,
+					Align: consts.Right,
+					Color: textSecondary,
+					Top:   2,
+				})
+			})
+		})
+
+		m.SetBackgroundColor(primaryColor)
+		m.Row(1, func() { m.Col(12, func() {}) })
+		m.SetBackgroundColor(tableHeaderBg)
+		m.Row(10, func() {
+			for i, header := range headers {
+				m.Col(widths[i], func() {
+					m.Text(header, props.Text{
+						Size:  11,
+						Style: consts.Bold,
+						Align: consts.Center,
+						Top:   2,
+						Color: secondaryColor,
+					})
+				})
+			}
+		})
+		m.SetBackgroundColor(subtleColor)
+		m.Row(0.5, func() { m.Col(12, func() {}) })
+		m.SetBackgroundColor(white)
+
+		useAlternateColor := false
+		for _, msg := range report.Messages {
+			if useAlternateColor {
+				m.SetBackgroundColor(alternateRow)
+			}
+
+			m.Row(9, func() {
+				m.Col(widths[0], func() {
+					m.Text(formatTime(msg.Time), props.Text{
+						Size:  10,
+						Align: consts.Center,
+						Top:   2,
+						Color: textPrimary,
+					})
+				})
+
+				m.Col(widths[1], func() {
+					m.Text(report.Metric.Name, props.Text{
+						Size:  10,
+						Style: consts.Bold,
+						Align: consts.Center,
+						Top:   2,
+						Color: primaryColor,
+					})
+				})
+
+				m.Col(widths[2], func() {
+					m.Text(formatValue(msg), props.Text{
+						Size:  10,
+						Style: consts.Normal,
+						Align: consts.Center,
+						Top:   2,
+						Color: textPrimary,
+					})
+				})
+
+				m.Col(widths[3], func() {
+					m.Text(msg.Unit, props.Text{
+						Size:  10,
+						Style: consts.Italic,
+						Align: consts.Center,
+						Top:   2,
+						Color: textSecondary,
+					})
+				})
+
+				m.Col(widths[4], func() {
+					m.Text(msg.Subtopic, props.Text{
+						Size:  10,
+						Align: consts.Center,
+						Top:   2,
+						Color: secondaryColor,
+					})
+				})
+			})
+
+			if !useAlternateColor {
+				m.Row(0.2, func() {
+					m.Col(12, func() {})
+				})
+			}
+
+			useAlternateColor = !useAlternateColor
+			m.SetBackgroundColor(white)
+		}
 	}
 
-	tmpl, err := template.New("report").Funcs(templateFuncs).Parse(reportTemplate)
+	buf, err := m.Output()
 	if err != nil {
-		return nil, fmt.Errorf("error parsing template: %v", err)
+		return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 	}
 
-	var htmlBuffer bytes.Buffer
-	err = tmpl.Execute(&htmlBuffer, TemplateData{Reports: templateReports})
-	if err != nil {
-		return nil, fmt.Errorf("error executing template: %v", err)
+	return buf.Bytes(), nil
+}
+
+func formatTime(t float64) string {
+	if t > 9999999999 {
+		return time.Unix(0, int64(t)*1000000).Format("2006-01-02 15:04:05")
 	}
+	return time.Unix(int64(t), 0).Format("2006-01-02 15:04:05")
+}
 
-	pdfg, err := wkhtmltopdf.NewPDFGenerator()
-	if err != nil {
-		return nil, fmt.Errorf("error creating PDF generator: %v", err)
+func formatValue(msg senml.Message) string {
+	switch {
+	case msg.Value != nil:
+		return fmt.Sprintf("%.2f", *msg.Value)
+	case msg.StringValue != nil:
+		return *msg.StringValue
+	case msg.BoolValue != nil:
+		return fmt.Sprintf("%t", *msg.BoolValue)
+	case msg.DataValue != nil:
+		return *msg.DataValue
+	default:
+		return "N/A"
 	}
-
-	pdfg.Dpi.Set(300)
-	pdfg.Orientation.Set(wkhtmltopdf.OrientationPortrait)
-	pdfg.PageSize.Set(wkhtmltopdf.PageSizeA4)
-	pdfg.MarginTop.Set(15)
-	pdfg.MarginBottom.Set(15)
-	pdfg.MarginLeft.Set(15)
-	pdfg.MarginRight.Set(15)
-	pdfg.Title.Set("Metrics Report")
-
-	page := wkhtmltopdf.NewPageReader(&htmlBuffer)
-	page.EnableLocalFileAccess.Set(true)
-	pdfg.AddPage(page)
-
-	err = pdfg.Create()
-	if err != nil {
-		return nil, fmt.Errorf("error generating PDF: %v", err)
-	}
-
-	return pdfg.Bytes(), nil
 }
 
 func generateCSVReport(reports []Report) ([]byte, error) {
@@ -217,37 +498,37 @@ func generateCSVReport(reports []Report) ([]byte, error) {
 
 		if i > 0 {
 			if err := writer.Write([]string{""}); err != nil {
-				return nil, err
+				return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 			}
 			if err := writer.Write([]string{"=== NEW REPORT ==="}); err != nil {
-				return nil, err
+				return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 			}
 			if err := writer.Write([]string{""}); err != nil {
-				return nil, err
+				return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 			}
 		}
 
 		if err := writer.Write([]string{"Report Information:"}); err != nil {
-			return nil, err
+			return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 		}
 		if err := writer.Write([]string{"Metric Name", report.Metric.Name}); err != nil {
-			return nil, err
+			return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 		}
 		if err := writer.Write([]string{"Device ID", report.Metric.ClientID}); err != nil {
-			return nil, err
+			return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 		}
 		if err := writer.Write([]string{"Channel ID", report.Metric.ChannelID}); err != nil {
-			return nil, err
+			return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 		}
 		if err := writer.Write([]string{"Subtopic", report.Metric.Subtopic}); err != nil {
-			return nil, err
+			return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 		}
 		if err := writer.Write([]string{""}); err != nil {
-			return nil, err
+			return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 		}
 
 		if err := writer.Write(headers); err != nil {
-			return nil, err
+			return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 		}
 
 		sort.Slice(report.Messages, func(i, j int) bool {
@@ -284,14 +565,14 @@ func generateCSVReport(reports []Report) ([]byte, error) {
 			}
 
 			if err := writer.Write(row); err != nil {
-				return nil, err
+				return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 			}
 		}
 	}
 
 	writer.Flush()
 	if err := writer.Error(); err != nil {
-		return nil, err
+		return nil, errors.Wrap(svcerr.ErrCreateEntity, err)
 	}
 
 	return buf.Bytes(), nil

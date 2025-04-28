f150b09af233becec60d29fc4e5db99febd9ee80
nyagamunene <stevenyaga2014@gmail.com>
2025-04-28 18:42:03 +0300

    adjust column sizes
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- re/generator.go
+++ re/generator.go
@@ -118,7 +118,7 @@ func generatePDFReport(reports []Report) ([]byte, error) {
 	})
 
 	headers := []string{"Time", "Device ID", "Protocol", "Name", "Value", "Unit", "Subtopic"}
-	widths := []uint{3, 4, 1, 2, 2, 1, 1}
+	widths := []uint{3, 3, 1, 1, 1, 1, 2}
 
 	for i, report := range reports {
 		if i > 0 {
@@ -218,9 +218,14 @@ func generatePDFReport(reports []Report) ([]byte, error) {
 				})
 
 				m.Col(widths[1], func() {
-					m.Text(msg.Publisher, props.Text{
+					deviceID := msg.Publisher
+					if len(deviceID) > 16 {
+						deviceID = deviceID[:13] + "..."
+					}
+
+					m.Text(deviceID, props.Text{
 						Size:  9,
-						Align: consts.Left,
+						Align: consts.Center,
 						Top:   2,
 						Color: textPrimary,
 					})

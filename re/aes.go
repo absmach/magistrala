030dbbacc42004a1ac6d9920a875121d5164e7a7
JonesKisaka <kisakajones11688@gmail.com>
2025-05-22 18:44:52 +0300

    AES Go script

--- re/aes.go
+++ re/aes.go
@@ -16,19 +16,19 @@ func decrypt(keyHex string, iv string, encryptedHex string) string {
 	/*
 	AES CBC-128 DECRYPTION requires 3 data fields
 	1. Key (16 bytes)
-	2. Initialization Vector (16 bytes)
-	3. Encrypted Data (16 bytes or length be multiple a of 16)
+	2. Initialization Vector (16 bytes). {Lua script generates this}
+	3. Encrypted Data (16 bytes or length be multiple a of 16) {Not the whole Telegram rather the encrypted part}
 
 	The encrypted data is divided into blocks of 16 bytes (128 bits) which then operated on with the IV and Key.   
 	*/
 	
-	//convert key hex string to bytes
+	// Convert key hex string to bytes
 	key,err := hex.DecodeString(strings.ReplaceAll(keyHex, " ", ""))
 	if err != nil {
 		log.Fatalf("Key Hex decode error: %v", err)
 	}
 
-	//convert encrypted hex string to bytes
+	// Convert encrypted hex string to bytes
 	encrypted,err := hex.DecodeString(strings.ReplaceAll(encryptedHex, " ", ""))
 	if err != nil {
 		log.Fatalf("Encrypted Hex decode error: %v", err)
@@ -42,26 +42,25 @@ func decrypt(keyHex string, iv string, encryptedHex string) string {
 			log.Fatalf("Failed to decode hex string: %v", err)
 		}
 		
-		//The IV bytes
+		// The IV bytes
 		iv_bytes = append(iv_bytes, newBytes...)
 		
-		//Create the Cipher from Key
+		// Create the Cipher from Key
 
 		block, err := aes.NewCipher(key)
 		if err != nil {
 			log.Fatalf("NewCipher error: %v", err)
 		}
 
-		//The encrypted block should be 16 bytes or a multiple of 16
+		// The encrypted block should be 16 bytes or a multiple of 16
 		if len(encrypted)%aes.BlockSize != 0 {
 			log.Fatalf("Encrypted data is not a multiple of the block size")
 		}
 
-		//decryption done with the key, IV and encrypted hex
+		// Decryption done with the key, IV and encrypted hex
 		mode := cipher.NewCBCDecrypter(block, iv_bytes)
 		decrypted := make([]byte, len(encrypted))
 		mode.CryptBlocks(decrypted, encrypted)
 
-	dec_result := fmt.Sprintf("Decrypted HEX: %X\n", decrypted)
-	return dec_result
+	return strings.ToUpper(hex.EncodeToString(decrypted))
 }

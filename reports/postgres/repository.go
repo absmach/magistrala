82e3e16b82aa190284ae2ef889a0ebcb4b3bbd15
nyagamunene <stevenyaga2014@gmail.com>
2025-07-11 19:28:06 +0300

    update method
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- reports/postgres/repository.go
+++ reports/postgres/repository.go
@@ -355,28 +355,21 @@ func (repo *PostgresRepository) ViewReportTemplate(ctx context.Context, domainID
 
 func (repo *PostgresRepository) DeleteReportTemplate(ctx context.Context, domainID, reportID string) error {
 	q := `
-		UPDATE report_config 
-		SET report_template = NULL, updated_at = :updated_at 
-		WHERE id = :id AND domain_id = :domain_id`
+        UPDATE report_config
+        SET report_template = '', updated_at = :updated_at
+        WHERE id = :id AND domain_id = :domain_id`
 
 	dbr := dbReport{
 		ID:        reportID,
 		DomainID:  domainID,
 		UpdatedAt: time.Now().UTC(),
 	}
-	result, err := repo.DB.ExecContext(ctx, q, dbr)
+	row, err := repo.DB.NamedQueryContext(ctx, q, dbr)
 	if err != nil {
-		return errors.Wrap(repoerr.ErrUpdateEntity, err)
+		return errors.Wrap(repoerr.ErrRemoveEntity, err)
 	}
 
-	rowsAffected, err := result.RowsAffected()
-	if err != nil {
-		return errors.Wrap(repoerr.ErrUpdateEntity, err)
-	}
-
-	if rowsAffected == 0 {
-		return repoerr.ErrNotFound
-	}
+	defer row.Close()
 
 	return nil
 }

2082a10eddc5f99bf19c0cedd684d91cbe2bd524
nyagamunene <stevenyaga2014@gmail.com>
2025-08-04 13:36:36 +0300

    remove walk node method
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- reports/template.go
+++ reports/template.go
@@ -8,8 +8,6 @@ import (
 	"fmt"
 	"text/template"
 	"text/template/parse"
-
-	"github.com/absmach/supermq/pkg/errors"
 )
 
 type ReportTemplate string
@@ -33,91 +31,105 @@ func (temp *ReportTemplate) UnmarshalJSON(data []byte) error {
 }
 
 func (temp ReportTemplate) Validate() error {
+	templateStr := string(temp)
+
+	// Validate template syntax using Go's template parser
 	tmpl := template.New("validate").Funcs(template.FuncMap{
 		"add":         func(a, b int) int { return a + b },
 		"formatTime":  func(t interface{}) string { return "" },
 		"formatValue": func(v interface{}) string { return "" },
 	})
 
-	parsedTmpl, err := tmpl.Parse(string(temp))
+	parsedTmpl, err := tmpl.Parse(templateStr)
 	if err != nil {
-		return fmt.Errorf("template parsing error: %w", err)
+		return fmt.Errorf("template syntax error: %w", err)
 	}
 
-	if err := walkNode(parsedTmpl.Tree.Root); err != nil {
+	// Validate essential fields are present using template parsing
+	if err := validateEssentialFields(parsedTmpl.Tree.Root); err != nil {
 		return err
 	}
 
 	return nil
 }
 
-func walkNode(node parse.Node) error {
-	switch n := node.(type) {
-	case *parse.ListNode:
-		for _, sub := range n.Nodes {
-			if err := walkNode(sub); err != nil {
-				return err
+func validateEssentialFields(node parse.Node) error {
+	var hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd bool
+
+	var traverse func(parse.Node)
+	traverse = func(n parse.Node) {
+		if n == nil {
+			return
+		}
+
+		switch node := n.(type) {
+		case *parse.ListNode:
+			for _, sub := range node.Nodes {
+				traverse(sub)
+			}
+
+		case *parse.ActionNode:
+			if node.Pipe != nil {
+				for _, cmd := range node.Pipe.Cmds {
+					cmdStr := cmd.String()
+					if cmdStr == "$.Title" {
+						hasTitle = true
+					}
+					if len(cmd.Args) > 0 {
+						firstArg := cmd.Args[0].String()
+						if firstArg == "formatTime" {
+							hasFormatTime = true
+						}
+						if firstArg == "formatValue" {
+							hasFormatValue = true
+						}
+					}
+				}
+			}
+
+		case *parse.RangeNode:
+			if node.Pipe != nil && len(node.Pipe.Cmds) > 0 {
+				cmdStr := node.Pipe.Cmds[0].String()
+				if cmdStr == ".Messages" {
+					hasRange = true
+				}
+			}
+			traverse(node.List)
+			if node.ElseList != nil {
+				traverse(node.ElseList)
+			}
+			hasEnd = true
+
+		case *parse.IfNode:
+			traverse(node.List)
+			if node.ElseList != nil {
+				traverse(node.ElseList)
+			}
+
+		case *parse.WithNode:
+			traverse(node.List)
+			if node.ElseList != nil {
+				traverse(node.ElseList)
 			}
 		}
+	}
 
-	case *parse.ActionNode:
-		fmt.Printf("Action: %s\n", n.String())
-		if len(n.Pipe.Cmds) == 0 {
-			return fmt.Errorf("empty action command: %s", n.String())
-		}
+	traverse(node)
 
-	case *parse.IfNode:
-		fmt.Printf("If block: %s\n", n.String())
-		if err := walkNode(n.List); err != nil {
-			return err
-		}
-		if n.ElseList != nil {
-			if err := walkNode(n.ElseList); err != nil {
-				return err
-			}
-		}
-
-	case *parse.RangeNode:
-		if n.Pipe != nil && len(n.Pipe.Cmds) == 1 {
-			cmdStr := n.Pipe.Cmds[0].String()
-			if cmdStr != ".Reports" && cmdStr != ".Messages" {
-				return errors.New("range allowed only on Reports or Messages")
-			}
-		}
-		if err := walkNode(n.List); err != nil {
-			return err
-		}
-		if n.ElseList != nil {
-			if err := walkNode(n.ElseList); err != nil {
-				return err
-			}
-		}
-
-	case *parse.TemplateNode:
-		fmt.Printf("Template call: %s\n", n.String())
-
-	case *parse.TextNode:
-		fmt.Printf("Text: %q\n", string(n.Text))
-
-	case *parse.WithNode:
-		fmt.Printf("With block: %s\n", n.String())
-		if err := walkNode(n.List); err != nil {
-			return err
-		}
-		if n.ElseList != nil {
-			if err := walkNode(n.ElseList); err != nil {
-				return err
-			}
-		}
-
-	case *parse.VariableNode:
-		fmt.Printf("Variable: %s\n", n.Ident)
-
-	case *parse.CommandNode:
-		fmt.Printf("Command: %s\n", n.String())
-
-	default:
-		return fmt.Errorf("unknown node type: %T", n)
+	if !hasTitle {
+		return fmt.Errorf("missing essential template field: {{$.Title}}")
+	}
+	if !hasRange {
+		return fmt.Errorf("missing essential template field: {{range .Messages}}")
+	}
+	if !hasFormatTime {
+		return fmt.Errorf("missing essential template field: {{formatTime .Time}}")
+	}
+	if !hasFormatValue {
+		return fmt.Errorf("missing essential template field: {{formatValue .}}")
+	}
+	if !hasEnd {
+		return fmt.Errorf("missing essential template field: {{end}}")
 	}
 
 	return nil

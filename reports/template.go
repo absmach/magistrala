189f141b394c64bbd193a02ab62ab4791a1d20e0
nyagamunene <stevenyaga2014@gmail.com>
2025-08-04 14:52:25 +0300

    address comments
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- reports/template.go
+++ reports/template.go
@@ -40,81 +40,16 @@ func (temp ReportTemplate) Validate() error {
 		"formatValue": func(v interface{}) string { return "" },
 	})
 
-	parsedTmpl, err := tmpl.Parse(templateStr)
+	parsed, err := tmpl.Parse(templateStr)
 	if err != nil {
 		return fmt.Errorf("template syntax error: %w", err)
 	}
 
+	var hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd bool
 	// Validate essential fields are present using template parsing
-	if err := validateEssentialFields(parsedTmpl.Tree.Root); err != nil {
+	if err := validateEssentialFields(parsed.Tree.Root, &hasTitle, &hasRange, &hasFormatTime, &hasFormatValue, &hasEnd); err != nil {
 		return err
 	}
-
-	return nil
-}
-
-func validateEssentialFields(node parse.Node) error {
-	var hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd bool
-
-	var traverse func(parse.Node)
-	traverse = func(n parse.Node) {
-		if n == nil {
-			return
-		}
-
-		switch node := n.(type) {
-		case *parse.ListNode:
-			for _, sub := range node.Nodes {
-				traverse(sub)
-			}
-
-		case *parse.ActionNode:
-			if node.Pipe != nil {
-				for _, cmd := range node.Pipe.Cmds {
-					cmdStr := cmd.String()
-					if cmdStr == "$.Title" {
-						hasTitle = true
-					}
-					if len(cmd.Args) > 0 {
-						firstArg := cmd.Args[0].String()
-						if firstArg == "formatTime" {
-							hasFormatTime = true
-						}
-						if firstArg == "formatValue" {
-							hasFormatValue = true
-						}
-					}
-				}
-			}
-
-		case *parse.RangeNode:
-			if node.Pipe != nil && len(node.Pipe.Cmds) > 0 {
-				cmdStr := node.Pipe.Cmds[0].String()
-				if cmdStr == ".Messages" {
-					hasRange = true
-				}
-			}
-			traverse(node.List)
-			if node.ElseList != nil {
-				traverse(node.ElseList)
-			}
-			hasEnd = true
-
-		case *parse.IfNode:
-			traverse(node.List)
-			if node.ElseList != nil {
-				traverse(node.ElseList)
-			}
-
-		case *parse.WithNode:
-			traverse(node.List)
-			if node.ElseList != nil {
-				traverse(node.ElseList)
-			}
-		}
-	}
-
-	traverse(node)
 
 	if !hasTitle {
 		return fmt.Errorf("missing essential template field: {{$.Title}}")
@@ -130,6 +65,79 @@ func validateEssentialFields(node parse.Node) error {
 	}
 	if !hasEnd {
 		return fmt.Errorf("missing essential template field: {{end}}")
+	}
+
+	return nil
+}
+
+func validateEssentialFields(node parse.Node, hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd *bool) error {
+	if node == nil {
+		return nil
+	}
+
+	switch n := node.(type) {
+	case *parse.ListNode:
+		for _, sub := range n.Nodes {
+			if err := validateEssentialFields(sub, hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd); err != nil {
+				return err
+			}
+		}
+
+	case *parse.ActionNode:
+		if n.Pipe != nil {
+			for _, cmd := range n.Pipe.Cmds {
+				cmdStr := cmd.String()
+				if cmdStr == "$.Title" {
+					*hasTitle = true
+				}
+				if len(cmd.Args) > 0 {
+					firstArg := cmd.Args[0].String()
+					if firstArg == "formatTime" {
+						*hasFormatTime = true
+					}
+					if firstArg == "formatValue" {
+						*hasFormatValue = true
+					}
+				}
+			}
+		}
+
+	case *parse.RangeNode:
+		if n.Pipe != nil && len(n.Pipe.Cmds) > 0 {
+			cmdStr := n.Pipe.Cmds[0].String()
+			if cmdStr == ".Messages" {
+				*hasRange = true
+			}
+		}
+		if err := validateEssentialFields(n.List, hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd); err != nil {
+			return err
+		}
+		if n.ElseList != nil {
+			if err := validateEssentialFields(n.ElseList, hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd); err != nil {
+				return err
+			}
+		}
+		*hasEnd = true
+
+	case *parse.IfNode:
+		if err := validateEssentialFields(n.List, hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd); err != nil {
+			return err
+		}
+		if n.ElseList != nil {
+			if err := validateEssentialFields(n.ElseList, hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd); err != nil {
+				return err
+			}
+		}
+
+	case *parse.WithNode:
+		if err := validateEssentialFields(n.List, hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd); err != nil {
+			return err
+		}
+		if n.ElseList != nil {
+			if err := validateEssentialFields(n.ElseList, hasTitle, hasRange, hasFormatTime, hasFormatValue, hasEnd); err != nil {
+				return err
+			}
+		}
 	}
 
 	return nil

f0dc63b65735166507421c49dd8c6ee4e869a740
ianmuchyri <ianmuchiri8@gmail.com>
2025-09-17 19:35:32 +0300

    allow support for multiple message options
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- re/outputs/slack.go
+++ re/outputs/slack.go
@@ -17,8 +17,30 @@ type Slack struct {
 }
 
 func (s *Slack) Run(ctx context.Context, msg *messaging.Message, val any) error {
+	b, err := json.Marshal(val)
+	if err != nil {
+		return err
+	}
+
+	var message slack.Msg
+	if err := json.Unmarshal(b, &message); err != nil {
+		return err
+	}
+
 	slackClient := slack.New(s.Token)
-	_, _, err := slackClient.PostMessage(s.ChannelID, slack.MsgOptionText(msg.String(), false))
+
+	var opts []slack.MsgOption
+
+	if message.Text != "" {
+		opts = append(opts, slack.MsgOptionText(message.Text, false))
+	}
+	if len(message.Attachments) > 0 {
+		opts = append(opts, slack.MsgOptionAttachments(message.Attachments...))
+	}
+	if len(message.Blocks.BlockSet) > 0 {
+		opts = append(opts, slack.MsgOptionBlocks(message.Blocks.BlockSet...))
+	}
+	_, _, err = slackClient.PostMessage(s.ChannelID, opts...)
 	if err != nil {
 		return err
 	}

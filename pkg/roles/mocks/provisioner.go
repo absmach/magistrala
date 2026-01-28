07aa10c1684f76ae9595cc6cca2a8f46fef39c95
Felix Gateru <felix.gateru@gmail.com>
2026-01-28 08:16:29 +0300

    feat: remove user from domain
    Signed-off-by: Felix Gateru <felix.gateru@gmail.com>

--- pkg/roles/mocks/provisioner.go
+++ pkg/roles/mocks/provisioner.go
@@ -215,3 +215,66 @@ func (_c *Provisioner_RemoveEntitiesRoles_Call) RunAndReturn(run func(ctx contex
 	_c.Call.Return(run)
 	return _c
 }
+
+// RemoveMemberFromDomain provides a mock function for the type Provisioner
+func (_mock *Provisioner) RemoveMemberFromDomain(ctx context.Context, domainID string, memberID string) error {
+	ret := _mock.Called(ctx, domainID, memberID)
+
+	if len(ret) == 0 {
+		panic("no return value specified for RemoveMemberFromDomain")
+	}
+
+	var r0 error
+	if returnFunc, ok := ret.Get(0).(func(context.Context, string, string) error); ok {
+		r0 = returnFunc(ctx, domainID, memberID)
+	} else {
+		r0 = ret.Error(0)
+	}
+	return r0
+}
+
+// Provisioner_RemoveMemberFromDomain_Call is a *mock.Call that shadows Run/Return methods with type explicit version for method 'RemoveMemberFromDomain'
+type Provisioner_RemoveMemberFromDomain_Call struct {
+	*mock.Call
+}
+
+// RemoveMemberFromDomain is a helper method to define mock.On call
+//   - ctx context.Context
+//   - domainID string
+//   - memberID string
+func (_e *Provisioner_Expecter) RemoveMemberFromDomain(ctx interface{}, domainID interface{}, memberID interface{}) *Provisioner_RemoveMemberFromDomain_Call {
+	return &Provisioner_RemoveMemberFromDomain_Call{Call: _e.mock.On("RemoveMemberFromDomain", ctx, domainID, memberID)}
+}
+
+func (_c *Provisioner_RemoveMemberFromDomain_Call) Run(run func(ctx context.Context, domainID string, memberID string)) *Provisioner_RemoveMemberFromDomain_Call {
+	_c.Call.Run(func(args mock.Arguments) {
+		var arg0 context.Context
+		if args[0] != nil {
+			arg0 = args[0].(context.Context)
+		}
+		var arg1 string
+		if args[1] != nil {
+			arg1 = args[1].(string)
+		}
+		var arg2 string
+		if args[2] != nil {
+			arg2 = args[2].(string)
+		}
+		run(
+			arg0,
+			arg1,
+			arg2,
+		)
+	})
+	return _c
+}
+
+func (_c *Provisioner_RemoveMemberFromDomain_Call) Return(err error) *Provisioner_RemoveMemberFromDomain_Call {
+	_c.Call.Return(err)
+	return _c
+}
+
+func (_c *Provisioner_RemoveMemberFromDomain_Call) RunAndReturn(run func(ctx context.Context, domainID string, memberID string) error) *Provisioner_RemoveMemberFromDomain_Call {
+	_c.Call.Return(run)
+	return _c
+}

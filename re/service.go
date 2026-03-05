0b15428028df7c4d8b1a408e6a946941980585a5
nyagamunene <stevenyaga2014@gmail.com>
2026-03-05 17:47:40 +0300

    revert code
    Signed-off-by: nyagamunene <stevenyaga2014@gmail.com>

--- re/service.go
+++ re/service.go
@@ -10,7 +10,6 @@ import (
 	grpcReadersV1 "github.com/absmach/magistrala/api/grpc/readers/v1"
 	"github.com/absmach/magistrala/pkg/emailer"
 	pkglog "github.com/absmach/magistrala/pkg/logger"
-	mgPolicies "github.com/absmach/magistrala/pkg/policies"
 	"github.com/absmach/magistrala/pkg/ticker"
 	"github.com/absmach/magistrala/re/operations"
 	"github.com/absmach/supermq"

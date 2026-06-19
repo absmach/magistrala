0bb8b0947a687fe855eee3b4e558c3bb6c383b2a
Arvindh <arvindh91@gmail.com>
2026-06-19 15:54:10 +0530

    Remove stale bootstrap backfill artifacts after Atom rebase
    Signed-off-by: Arvindh <arvindh91@gmail.com>

--- pkg/bootstrap/events/consumer/doc.go
+++
@@ -1,6 +0,0 @@
-// Copyright (c) Abstract Machines
-// SPDX-License-Identifier: Apache-2.0
-
-// Package consumer contains events consumer for client and channel events
-// consumed by the Bootstrap service.
-package consumer

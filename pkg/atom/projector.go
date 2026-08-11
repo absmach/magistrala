702e70a08ebfcbe4e6f5a8feb983b90dca2f6a47
Ian Ngethe Muchiri <100555904+ianmuchyri@users.noreply.github.com>
2026-08-11 09:52:35 +0300

    Add group membership, hierarchy and typed creation to atom client (#3567)
    Signed-off-by: ianmuchyri <ianmuchiri8@gmail.com>

--- pkg/atom/projector.go
+++ pkg/atom/projector.go
@@ -8,7 +8,6 @@ import "context"
 type Projector interface {
 	UpsertTenant(ctx context.Context, tenant Tenant) error
 	UpsertEntity(ctx context.Context, entity Entity) error
-	UpsertGroup(ctx context.Context, group Group) error
 	UpsertResource(ctx context.Context, resource Resource) error
 	DeleteTenant(ctx context.Context, id string) error
 	DeleteEntity(ctx context.Context, id string) error
